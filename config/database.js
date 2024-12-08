0632630e58a20969680af531098aab73915281e7
Mayank Harplani <130099877+mayankharplani@users.noreply.github.com>
2024-12-08 15:03:18 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,13 +3,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306
-});
+const dbUri = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+const db = mysql.createConnection(dbUri);
+
 
 db.connect((err) => {
     if (err) {
